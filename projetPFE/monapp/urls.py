9f9a302c49f876762202415f8329b18c73d604d5
ramzirn <ramzibouter6@gmail.com>
2024-03-25 14:48:51 +0100

    25/03/2024:14:48 , Django App index page fully implemented rest other pages

--- projetPFE/monapp/urls.py
+++ projetPFE/monapp/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
     path('connexion/', views.login, name='login'),
-    # Ajoutez d'autres URL si nécessaire
+    path('about/', views.about, name='about'),
+    path('blog-details/', views.blog_details, name='blog_details'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('accueil/', views.index, name='index'),
+    path('job-details/', views.job_details, name='job_details'),
+    path('team/', views.team, name='team'),
+    path('terms/', views.terms, name='terms'),
+    path('jobs/', views.jobs, name='jobs'),
 ]
