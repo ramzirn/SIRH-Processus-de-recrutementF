a599b3f49dbe8ac89fb9aa218d492413d669263d
ramzirn <ramzibouter6@gmail.com>
2024-04-04 11:45:20 +0100

    Grille de notation works

--- job_agency/models/FicheCandidat.py
+++ job_agency/models/FicheCandidat.py
@@ -1,5 +1,4 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
 
 class Employee(models.Model):
     _name = 'candidat'
@@ -19,9 +18,14 @@ class Employee(models.Model):
     diplome_id = fields.Many2one('hr.recruitment.degree', string='Diplôme')
     specialite = fields.Char(string='Spécialité')
 
-    def name_get(self):
-        result = []
+    savoir_et_connaissance = fields.Integer(string='Savoir et Connaissance', default=0)
+    savoir_faire_et_experience = fields.Integer(string='Savoir-Faire et Expérience', default=0)
+    savoir_etre_et_qualite_requises = fields.Integer(string='Savoir-Être et Qualité Requises', default=0)
+    formations_diplome = fields.Integer(string='Formations et Diplôme', default=0)
+    total_points = fields.Integer(string='Total des Points', compute='_compute_total_points', store=True)
+    observation = fields.Text(string='Observation')
+
+    @api.depends('savoir_et_connaissance', 'savoir_faire_et_experience', 'savoir_etre_et_qualite_requises', 'formations_diplome')
+    def _compute_total_points(self):
         for record in self:
-            name = "%s %s" % (record.nom, record.prenom)
-            result.append((record.id, name))
-        return result
+            record.total_points = record.savoir_et_connaissance + record.savoir_faire_et_experience + record.savoir_etre_et_qualite_requises + record.formations_diplome
