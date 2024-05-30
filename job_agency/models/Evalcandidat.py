3acb59e6e650180226e8c6850ed075178181320c
ramzirn <ramzibouter6@gmail.com>
2024-05-30 11:14:36 +0100

    30/05/2024 11:14 , Evaluation candidat done

--- job_agency/models/Evalcandidat.py
+++ job_agency/models/Evalcandidat.py
@@ -5,12 +5,13 @@ class CandidateEvaluation(models.Model):
     _name = 'candidate.evaluation'
     _description = 'Candidate Evaluation'
 
-    candidate_name = fields.Char(string='Candidat')
+    candidate_name = fields.Many2one('hr.applicant', string='Candidat')
+
     interview_date = fields.Date(string="Date d'entretien")
     position = fields.Char(string='Poste à pourvoir')
 
     tech_competence_points = fields.Integer(string='Points forts (Compétences techniques)')
-    tech_competence_develop = fields.Integer(string='Points à développer (Compétences techniques)')
+    tech_competence_develop = fields.Text(string='Points à développer (Compétences techniques)')
     tech_competence_note = fields.Selection([(str(num), str(num)) for num in range(1, 6)], string='Note (1 à 5)')
 
     teamwork_capacity = fields.Integer(string='Capacité de travailler en équipe')
@@ -18,8 +19,8 @@ class CandidateEvaluation(models.Model):
     decision_capacity = fields.Integer(string='Capacité à répondre des décisions')
     emotion_management = fields.Integer(string='Capacité à gérer ses émotions')
 
-    analysis_capacity_points = fields.Integer(string='Points forts (Capacité d\'analyse)')
-    analysis_capacity_develop = fields.Integer(string='Points à développer (Capacité d\'analyse)')
+    analysis_capacity_points = fields.Text(string='Points forts (Capacité d\'analyse)')
+    analysis_capacity_develop = fields.Text(string='Points à développer (Capacité d\'analyse)')
     analysis_capacity_note = fields.Selection([(str(num), str(num)) for num in range(1, 6)], string='Note (1 à 5)')
 
     synthesis_capacity = fields.Integer(string='Esprit de synthèse')
